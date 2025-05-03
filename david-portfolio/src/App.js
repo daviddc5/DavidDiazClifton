b6bf50f63e488ed289300f7999e40b19a953c750
VindicoDavid <daviddiazclifton@gmail.com>
2025-05-03 20:45:24 +0200

    initial version

---
+++ david-portfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NintendoSwitchPortfolio from "./components/NintendoSwitchPortfolio";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <NintendoSwitchPortfolio />
+    </div>
+  );
+}
+
+export default App;
